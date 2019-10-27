44849d68b8772cc4a5ba9312bb298c3bfe1329aa
Yue Yang <g1enyy0ung@gmail.com>
2019-10-27 11:02:12 +0800

    Update

--- src/components/Table/TableHeader.jsx
+++ src/components/Table/TableHeader.jsx
@@ -9,17 +9,28 @@ const TableHeader = props => {
         <th>Cluster</th>
         {item.inst_name && <th>Instance</th>}
         <th>DB</th>
-        {item.count && <th>Count</th>}
+        {item.count && (
+          <th onClick={props.handleSortTable((a, b) => a.count - b.count, (a, b) => b.count - a.count)}>
+            <i className="fas fa-sort has-text-primary" /> Count
+          </th>
+        )}
         <th onClick={props.handleSortTable((a, b) => a.sql_id - b.sql_id, (a, b) => b.sql_id - a.sql_id)}>
           <i className="fas fa-sort has-text-primary" /> SQL Id
         </th>
         <th>SQL Text</th>
         <th>Datetime</th>
-        <th onClick={props.handleSortTable((a, b) => a.query_time - b.query_time, (a, b) => b.query_time - a.query_time)}>
+        <th
+          onClick={props.handleSortTable((a, b) => a.query_time - b.query_time, (a, b) => b.query_time - a.query_time)}
+        >
           <i className="fas fa-sort has-text-primary" /> Query Time
         </th>
         <th>Process Keys</th>
-        <th onClick={props.handleSortTable((a, b) => a.process_time - b.process_time, (a, b) => b.process_time - a.process_time)}>
+        <th
+          onClick={props.handleSortTable(
+            (a, b) => a.process_time - b.process_time,
+            (a, b) => b.process_time - a.process_time
+          )}
+        >
           <i className="fas fa-sort has-text-primary" /> Process Time
         </th>
         <th>Commit Time</th>
